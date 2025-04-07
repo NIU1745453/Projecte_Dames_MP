e3ff0585d7b1210b9a2ca0a993d4927397feccb0
Daria <DariaMaria.Radoi@autonoma.cat>
2025-04-07 12:18:51 +0200

    Editar-funcions

--- Fitxa.cpp
+++ Fitxa.cpp
@@ -1,1 +1,10 @@
-#include "Fitxa.h""
+#include "Fitxa.h"
+
+//aquesta funcio ha de condiferer tots els moviments que podra fer la fitxa des de la seva posicio acual
+//respecte el tauler sencer i haura de guardar en un array un conjunt d'arrays que guardaran els
+//possibles moviments que pot fer la fitxa
+//P(2a) = [3c, (3c,4e)], este formato vamos
+Moviment Fitxa::movimentsValids()
+{
+
+}
--- Moviment.h
+++ Moviment.h
@@ -1,11 +1,22 @@
 #include <iostream>
 #include <string.h>
 
-const int MAX_MOV = 2; // indica les coordenades del moviment
 
 class Moviment
 {
 public:
+	Moviment() : m_moviment(""), m_horitzontal(0456), m_vertical(0){}
+	Moviment(string mov) : m_moviment(mov) {}
+	void getMoviment() { return m_moviments; }
+	string setMoviment(string mov) { m_moviment = mov; }
+
+	bool compatible(int vertical, int horitzontal); //mirara si el moviment que mira es pot fer o no,
+													// en cas de que es pugui fer el mov, l'haura de guardar per
+													//tal de poderlo tenir en l'array moviments possibles
+
+
 private:
-	int m_moviment[MAX_MOV];
+	string m_moviment;
+	int m_horitzontal;
+	int m_vertical;
 };
